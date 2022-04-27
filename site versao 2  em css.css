7ccd0bc1cd4128684eb7718b23da32ea35b4f022
juliapaiva1 <104441000+juliapaiva1@users.noreply.github.com>
2022-04-27 14:50:50 -0300

    Add files via upload

---
+++ site versao 2  em css.css
@@ -0,0 +1,36 @@
+body {
+
+    background: #73C2FB
+}
+
+H1 {
+    font-size: 50px;
+    margin-top: 15px;
+    margin-left: 5px;
+}
+
+img, H1 {
+    display: inline-block;
+    margin-left: 20px;
+}
+
+H3 {
+    margin-left: 25px;
+    width: 28%;
+    font-family: "Century Gothic";
+    background: #FC6A03
+}
+
+h2 {
+    font-family: "Trebuchet MS";
+    margin-top: -30px;
+    margin-left: 95px;
+    margin-bottom: 30px;
+}
+
+p {
+    font-family: Arial;
+    margin-left: 25px;
+    margin-top: -10px;
+    margin-bottom: 28px;
+}
